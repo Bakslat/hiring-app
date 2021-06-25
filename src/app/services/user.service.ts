2e6bd4719338286d01fbc35ffe3587d7a95165cb
Kristiyan Kalchev <kristiyan.kalchev@pliant.io>
2021-06-25 15:37:43 +0300

    Added login functionality

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { User } from '../models/user.model';
+import { environment } from 'src/environments/environment';
+
+import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
+
+  login(email: string, password: string) {
+    console.log(email, password);
+    return this.http.post<any>(`${environment.apiUrl}/login`, {"email": email, "password": password})
+      .pipe(map(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+        return user;
+    }));
+  }
+
+}
